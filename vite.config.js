c228229dcd4fa12db08875463cb40490d61cded9
ArtemGoiT <150847319+CodeByArtem@users.noreply.github.com>
2025-01-11 13:55:30 +0100

    Update index.html

--- vite.config.js
+++ vite.config.js
@@ -1,56 +1,3 @@
-// import { defineConfig } from "vite";
-// import path from "path";
-// import glob from "fast-glob";
-// import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
-// import { fileURLToPath } from "url";
-// import imagemin from "imagemin";
-// import imageminWebp from "imagemin-webp";
-// import purgecss from "@fullhuman/postcss-purgecss"; 
-
-// export default defineConfig({
-//   base: '/my-sass-project/',
-//   plugins: [
-//     ViteImageOptimizer({
-//       png: { quality: 86 },
-//       jpeg: { quality: 86 },
-//       jpg: { quality: 86 },
-//     }),
-//     {
-//       ...imagemin(["./src/img/**/*.{jpg,png,jpeg}"], {
-//         destination: "./src/img/webp/",
-//         plugins: [imageminWebp({ quality: 86 })],
-//       }),
-//       apply: "serve",
-//     },
-//   ],
-//   build: {
-//     minify: false,
-//     rollupOptions: {
-//       input: Object.fromEntries(
-//         glob
-//           .sync(["./*.html", "./pages/**/*.html"])
-//           .map((file) => [
-//             path.relative(__dirname, file.slice(0, file.length - path.extname(file).length)),
-//             fileURLToPath(new URL(file, import.meta.url)),
-//           ])
-//       ),
-//       output: {
-//         assetFileNames: "assets/[name].[ext]",
-//       },
-//     },
-//   },
-//   // PostCSS конфігурація для purgecss
-//   css: {
-//     postcss: {
-//       plugins: [
-//         purgecss({
-//           content: ["./**/*.html"], 
-//         }),
-//       ],
-//     },
-//   },
-// });
-
 import imagemin from "imagemin";
 import imageminWebp from "imagemin-webp";
 import path from "path";
@@ -58,10 +5,8 @@ import { defineConfig } from "vite";
 import glob from "fast-glob";
 import { fileURLToPath } from "url";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
-import purgecss from "@fullhuman/postcss-purgecss";
 
 export default defineConfig({
-  base: '/lesa/',
   plugins: [
     ViteImageOptimizer({
       png: {
@@ -81,10 +26,6 @@ export default defineConfig({
       }),
       apply: "serve",
     },
-    purgecss({
-      content: ['./**/*.html', './**/*.js'],
-      safelist: ['is-open'],
-    }),
   ],
   build: {
     minify: false, // disable minification
@@ -93,10 +34,7 @@ export default defineConfig({
         glob
           .sync(["./*.html", "./pages/**/*.html"])
           .map((file) => [
-            path.relative(
-              __dirname,
-              file.slice(0, file.length - path.extname(file).length)
-            ),
+            path.relative(__dirname, file.slice(0, file.length - path.extname(file).length)),
             fileURLToPath(new URL(file, import.meta.url)),
           ])
       ),
